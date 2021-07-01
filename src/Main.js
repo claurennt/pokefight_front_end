35dfa5593b794eb340b77f190dfd8d23c7163323
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-07-01 16:09:03 +0200

    install

---
+++ src/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ChooseFighter from "./src/components/ChooseFighter";
+import FighterPreview from "./src/components/Fighterpreview";
+import FighterDetailed from "./src/components/FighterDetailed";
+import Fight from "./src/components/Fight";
+export default function Main() {
+  return (
+    <main>
+      <Switch>
+        <Route path="/">
+          <ChooseFighter />
+        </Route>
+        <Route path="/:id">
+          <FighterPreview />
+        </Route>
+        <Route path="/:id/:info">
+          <FighterDetailed />
+        </Route>
+        <Route path="/fight">
+          <Fight />
+        </Route>
+      </Switch>
+    </main>
+  );
+}
